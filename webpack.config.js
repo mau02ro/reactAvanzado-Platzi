50cbfa576c90d16784358222eec8330ab95bcafe
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-08-06 15:18:53 -0430

    React  Done

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,23 @@ module.exports = {
   output: {
     filename: "app.bundle.js",
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(js||jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
 };
