b5278bb824c032586bdda79d0312468c72fa30d9
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-08-07 16:29:04 -0430

    IntersectionObserver

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { Article, Img, ImgWrapper, Button } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 export const PhotoCard = ({ id, likes = 0, src }) => {
-  return (
-    <Article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} alt='PhotoCard' />
-        </ImgWrapper>
-      </a>
+  const ref = useRef(null)
+  const [show, setShow] = useState(false)
 
-      <Button>
-        <MdFavoriteBorder size='30px' />{likes} likes!
-      </Button>
+  useEffect(() => {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver : import('intersection-observer')
+    )
+      .then(() => {
+        const observer = new window.IntersectionObserver((entires) => {
+          const { isIntersecting } = entires[0]
+          if (isIntersecting) {
+            setShow(true)
+            observer.disconnect()
+          }
+        })
+
+        observer.observe(ref.current)
+      })
+  }, [ref])
+
+  return (
+    <Article ref={ref}>
+      {
+        show && (
+          <>
+            <a href={`/detail/${id}`}>
+              <ImgWrapper>
+                <Img src={src} alt='PhotoCard' />
+              </ImgWrapper>
+            </a>
+
+            <Button>
+              <MdFavoriteBorder size='30px' />{likes} likes!
+            </Button>
+          </>
+        )
+      }
     </Article>
   )
 }
