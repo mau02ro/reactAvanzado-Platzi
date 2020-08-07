1d169c3c2c4a7ba87135f28f8f7c0836a746e965
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-08-06 22:01:34 -0430

    Logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 const App = () => {
   return (
     <div>
+      <Logo />
       <GlobalStyle />
       <ListOfCategories />
       <ListOfPhotoCards />
