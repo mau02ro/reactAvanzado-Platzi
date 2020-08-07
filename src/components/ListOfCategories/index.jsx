d4089ec86675e07f9ad21c909dc2938e11ac61b1
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-08-07 15:55:30 -0430

    ListOfPhotos

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+
+function useCategoriesData () {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    window.fetch('https://petgram-server-mau02ro.vercel.app/categories')
+      .then((res) => {
+        setLoading(true)
+        return res.json()
+      })
+      .then((res) => {
+        setCategories(res)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setLoading(false)
+
+        console.error(err)
+      })
+  }, [])
+  return { categories, loading }
+}
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [showFixed, setShowFixed] = useState(false)
+  const { categories, loading } = useCategoriesData()
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
         categories.map((categorie) => {
           return (
@@ -14,5 +49,16 @@ export const ListOfCategories = () => {
         })
       }
     </List>
+  )
+
+  if (loading) {
+    return 'Cargando...'
+  }
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
